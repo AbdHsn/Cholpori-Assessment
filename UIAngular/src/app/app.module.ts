45d8f16dfbe54005c62f7f20d5113917c193a2bc
AbdHsn-Laptop <AbdHsn-Laptop@ABDHSN-LAPTOP>
2022-05-30 09:41:52 +0600

    ddd

--- UIAngular/src/app/app.module.ts
+++ UIAngular/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeContentComponent } from './components/pages/home-content/home-content.component';
+import { NgxNavbarModule } from 'ngx-bootstrap-navbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeContentComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, NgxNavbarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ UIAngular/src/app/components/header/header.component.ts
@@ -0,0 +1,19 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DoCheck,
+  OnInit,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements DoCheck {
+  constructor() {}
+  ngDoCheck() {
+    console.count('checked');
+  }
+}
